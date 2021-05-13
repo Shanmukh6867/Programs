41d0790f795952694f2c03b3d761fd7700362a99
Shanmukh6867 <84019742+Shanmukh6867@users.noreply.github.com>
2021-05-13 09:25:50 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+  printf("this is the program which is directly saved in git\n");
+  int a=5,b=6,c=0;
+  c=a+b;
+  printf("adding a and b the result is %d\n",c);
+}
